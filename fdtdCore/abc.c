6018ad30cffb897d73b9c06f072257573589a42a
Aurelien Duval <aurelien.duval@optiwave.com>
2014-12-05 08:34:39 -0500

    Implement 2nd order ABC

--- fdtdCore/abc.c
+++ fdtdCore/abc.c
@@ -5,45 +5,75 @@
 #include <math.h>
 
 static int initDone = 0;
-static double ezOldLeft = 0.0;
-static double ezOldRight = 0.0;
-static double abcCoefLeft;
-static double abcCoeRight;
+static double *ezOldLeft1;
+static double *ezOldLeft2;
+static double *ezOldRight1;
+static double *ezOldRight2;
+static double *abcCoefLeft;
+static double *abcCoefRight;
 
-//initializes the ABC (1st order diff equation)
+//initializes the ABC (2nd order diff equation or Mur ABC)
 void abcInit(Grid *g)
 {
-	double temp;
+	double temp1, temp2;
+
+	ALLOC_1D(ezOldLeft1, 3, double);
+	ALLOC_1D(ezOldLeft2, 3, double);
+	ALLOC_1D(ezOldRight1, 3, double);
+	ALLOC_1D(ezOldRight2, 3, double);
+	ALLOC_1D(abcCoefLeft, 3, double);
+	ALLOC_1D(abcCoefRight, 3, double);
+
 
 	/* left boundary coefficient */
-	temp = sqrt(Cezh(0) * Chye(0));
-	abcCoefLeft = (temp - 1.0) / (temp + 1.0);
+	temp1 = sqrt(Cezh(0) * Chye(0));
+	temp2 = 1.0 / temp1 + 2.0 + temp1;
+	abcCoefLeft[0] = -(1.0 / temp1 - 2.0 + temp1) / temp2;
+	abcCoefLeft[1] = -2.0 * (temp1 - 1.0 / temp1) / temp2;
+	abcCoefLeft[2] =  4.0 * (temp1 + 1.0 / temp1) / temp2;
+
 
 	/* right boundary coefficient */
-	temp = sqrt(Cezh(SizeX -1) * Chye(SizeX -2));
-	abcCoeRight = (temp - 1.0) / (temp + 1.0);
+	temp1 = sqrt(Cezh(SizeX -1) * Chye(SizeX -2));
+	temp2 = 1.0 / temp1 + 2.0 + temp1;
+	abcCoefRight[0] = -(1.0 / temp1 - 2.0 + temp1) / temp2;
+	abcCoefRight[1] = -2.0 * (temp1 - 1.0 / temp1) / temp2;
+	abcCoefRight[2] = 4.0 * (temp1 + 1.0 / temp1) / temp2;
 
 	initDone = 1;
 	return;
 }
 
-//apply the ABC -- left side only
+//apply the ABC
 void abc(Grid *g)
 {
+	int mm ;
+
 	if (!initDone) {
 		fprintf(stderr, "abc: uninitialized boundaries, call abcInit first.\n");
 		exit(-1);
 	}
 
 	/* left boundary */
-	Ez(0) =			ezOldLeft + 
-					abcCoefLeft * (Ez(1) - Ez(0));
-	ezOldLeft = Ez(1);
+	Ez(0) =		abcCoefLeft[0] * (Ez(2) + ezOldLeft2[0])
+			+	abcCoefLeft[1] * (ezOldLeft1[0] + ezOldLeft1[2] - Ez(1) - ezOldLeft2[1])
+			+	abcCoefLeft[2] *  ezOldLeft1[1] - ezOldLeft2[2];
+								;
 
 	/* right boundary */
-	Ez(SizeX -1) =	ezOldRight + 
-					abcCoeRight * (Ez(SizeX -2) - Ez(SizeX - 1));
-	ezOldRight = Ez(SizeX - 2);
+	Ez(SizeX-1) =	abcCoefRight[0] * (Ez(SizeX -3) + ezOldRight2[0])
+				+	abcCoefRight[1] * (ezOldRight1[0] + ezOldRight1[2] - Ez(SizeX-2) - ezOldRight2[1])
+				+	abcCoefRight[2] * ezOldRight1[1] - ezOldRight2[2];
+
+	/* update old fields */
+	for (mm = 0; mm < 3; mm++)
+	{
+		ezOldLeft2[mm] = ezOldLeft1[mm];
+		ezOldLeft1[mm] = Ez(mm);
+
+		ezOldRight2[mm] = ezOldRight1[mm];
+		ezOldRight1[mm] = Ez(SizeX -1 - mm);
+	}
 
 	return;
 }
